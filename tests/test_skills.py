830477ebc96082315f58d548f3b37bf12afc0694
Taylor Agarwal <taagarwa@redhat.com>
2026-04-14 17:57:39 -0400

    ✅ Add tests with real tool use

--- tests/test_skills.py
+++ tests/test_skills.py
@@ -1,6 +1,9 @@
+import os
 import pytest
 
-from tests.assertions import assert_skill_activated, assert_passes
+from claude_agent_sdk import ClaudeAgentOptions
+
+from tests.assertions import assert_skill_activated, assert_passes, assert_tool_called
 from tests.harness import run_query    
     
 class TestContextFetcher:
@@ -8,9 +11,14 @@ class TestContextFetcher:
     @pytest.fixture
     def agent_input(self):
         return "Activate the context-fetcher skill"
-
-    async def test_skill_activation(self, agent_input, default_agent_options):
-        messages = await run_query(input=agent_input, options=default_agent_options)
+    
+    @pytest.fixture
+    def agent_options(self, default_agent_options: ClaudeAgentOptions):
+        options = default_agent_options
+        return options
+    
+    async def test_skill_activation(self, agent_input, agent_options):
+        messages = await run_query(input=agent_input, options=agent_options)
         pytest.messages = messages
         assert_skill_activated(messages=messages, expected_skill="rhdp-rca-plugin:context-fetcher")
         
@@ -19,3 +27,37 @@ class TestContextFetcher:
         assertion = "The assistant was friendly and helpful"
         messages = pytest.messages
         await assert_passes(input=agent_input, messages=messages, assertion=assertion)    
+
+
+class TestContextFetcherSkillGitHubSearch:
+    
+    @pytest.fixture
+    def agent_input(self):
+        return "Search github for taagarwa-rh's prompt optimization project and tell me what algorithms are available there."
+    
+    @pytest.fixture
+    def agent_options(self, default_agent_options: ClaudeAgentOptions):
+        options = default_agent_options
+        options.mcp_servers = {
+            "github": {
+                "type": "http",
+                "url": "https://api.githubcopilot.com/mcp",
+                "headers": {
+                    "Authorization": f"Bearer {os.environ["GITHUB_TOKEN"]}"
+                }
+            }
+        }
+        allowed_tools = ["mcp__github__search_code", "mcp__github__get_file_contents", "mcp__github__search_repositories"]
+        options.tools.extend(allowed_tools)
+        options.allowed_tools.extend(allowed_tools)
+        return options
+    
+    async def test_skill_activation(self, agent_input, agent_options):
+        messages = await run_query(input=agent_input, options=agent_options)
+        pytest.messages = messages
+        assert_skill_activated(messages=messages, expected_skill="rhdp-rca-plugin:context-fetcher")
+        
+    @pytest.mark.dependency("test_skill_activation")
+    async def test_search_repositories_called(self):
+        messages = pytest.messages
+        await assert_tool_called(messages=messages, expected_tool="mcp__github__search_repositories")
